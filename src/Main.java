ebc94866f4035128e194b22dc6ddc8a43806dd9c
Shahzad Khan <75515143+shahzad004@users.noreply.github.com>
2025-08-27 13:21:28 -0700

    new line

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,9 @@
 public class Main {
 
 
-    public static void Sum(int a, int b){
-;
-        System.out.println((a+b) );
-    }
+
     public static void main(String[] args) {
 
-        Sum(10,20);
+        System.out.println("Hello How are you , Shahzad");
     }
 }
