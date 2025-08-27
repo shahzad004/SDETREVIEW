01057dd695fa67cd80bce0fdd733de9348c62e5a
Shahzad Khan <75515143+shahzad004@users.noreply.github.com>
2025-08-27 13:50:57 -0700

    shahzad Codeb

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
 
 
 
-    public static void main(String[] args) {
+    public class Main {
 
-        System.out.println("Hello How are you , Shahzad");
-    }
+
+        public static int Sum(int a, int b) {
+            int sum = a + b;
+            System.out.println("sum of A + B is : " + sum);
+            return sum;
+        }
+
+        public static void main(String[] args) {
+            Sum(20, 50);
+            Sum(20, 90);
+            Sum(20, 30);
+        }
 }
